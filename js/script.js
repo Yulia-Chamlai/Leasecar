fd7a7dbafe82f32d34f66eed7e2497ed973c7a8a
Yulia-Chamlai <yuliachamlai@gmail.com>
2023-06-28 16:13:44 +0300

    init repo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const tabItem = document.querySelectorAll(".cars__btn");
+const tabContent = document.querySelectorAll(".card__row");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function (item) {
+    item.classList.remove("cars__btn--active");
+  });
+  tabTarget.classList.add("cars__btn--active");
+  tabContent.forEach(function (item) {
+    item.classList.remove("card__row--active");
+  });
+  document.querySelector(`#${button}`).classList.add("card__row--active");
+}
+
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
